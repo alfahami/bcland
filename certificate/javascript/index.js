9cafb49a8177f4f306c8bd1803e835e608693cc1
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2022-06-26 13:54:14 +0100

    shell+admin-service

---
+++ certificate/javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const TitreContract = require('./lib/titreContract');
+
+module.exports.TitreContract = TitreContract;
+module.exports.contracts = [ TitreContract ];
